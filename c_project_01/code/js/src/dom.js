8c0111704556900eaf97774a14647b7307ecc3fc
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-04-26 10:23:02 +0900

    dom의 개념

---
+++ c_project_01/code/js/src/dom.js
@@ -0,0 +1,14 @@
+// dom.js
+
+// Document(html에서 body라고 불리우는 존재) Of Model
+// DOM : 웹페이지 화면에 보이는 모든 것(html, css)을 제어하는 행동을 DOM 처리
+// BOM : brower of model : 브라우저 자체에 있는 모든 것
+
+// html : 구조, css : 디자인, js : 제어
+
+// js
+// 1. 무엇을 하던 무조건 수동, DIY, 가내수공업
+// 2. 생성 - 내용 작성 - 배치
+// 3. html 문서가 먼저 배치가 된 이후 js를 사용해야 html 문서를 인식
+// 4. js에서 순서를 고려해서 코딩을 짜야 동작
+
